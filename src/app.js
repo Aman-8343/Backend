301a05869af7a5ffa33dfe934e09babdefaabdd4
Aman kumar <amankumar105541@gmail.com>
2025-03-25 21:35:05 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -10,5 +10,7 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"100kb"}))
-
+app.use(express.urlencoded({extended:true,limit:"100kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 export {app}
