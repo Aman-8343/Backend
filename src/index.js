02ae61fb196cd262f347835bd0f939cf3907e6fb
Aman kumar <amankumar105541@gmail.com>
2025-03-25 13:04:22 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -10,8 +10,15 @@ dotenv.config({
 
 
 
-connectDB();
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Sever is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongo db connection failed!!!",err)
+})
 
 
 
