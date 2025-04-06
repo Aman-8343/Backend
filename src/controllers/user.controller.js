06e4687c7fd496cfa87a6060908842770df47f29
Aman kumar <amankumar105541@gmail.com>
2025-04-06 12:10:35 +0530

    steps for register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,8 @@
+import { exists } from "fs";
 import { asynchandler } from "../utils/asynchandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { Db } from "mongodb";
+import { response } from "express";
 
 const registerUser= asynchandler(  async (req,res)=>{
     res.status(200).json({
@@ -8,3 +12,14 @@ const registerUser= asynchandler(  async (req,res)=>{
 
 
 export {registerUser}
+
+
+// get user detail from frontend
+// vaidation - not empty
+// check if the user is already exists 
+// check for the images, check for the avatar
+// uploadOnCloudinary ,avatar
+// cretae user Object- create entry in Db
+// remove password and refresh token frmm response
+// check for user creation
+// return response.
