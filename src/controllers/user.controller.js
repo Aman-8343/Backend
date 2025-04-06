99398ce33bea362fdabc724f4100643e6ddfb22c
Aman kumar <amankumar105541@gmail.com>
2025-04-06 22:24:17 +0530

    checking validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import { asynchandler } from "../utils/asynchandler.js";
-
+import { apiError} from "../utils/apiError.js"
 
 const registerUser= asynchandler(  async (req,res)=>{
   const {fullName,email,username,password}=  req.body
-  console.log("email",email)
+  console.log("email",email);
+
+if(
+    [fullName,email,username,password].some((field)=>
+      field?.trim()===""
+)
+){
+    throw new apiError(400,"all fields are required ")
+}
+
 })
 
 
