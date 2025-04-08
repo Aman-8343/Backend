497535c35f47848c91027ab6af4111249695374b
Aman kumar <amankumar105541@gmail.com>
2025-04-08 22:13:43 +0530

    logout User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -135,7 +135,27 @@ return res
 
 
 const logoutUser=asynchandler(async(req,res)=>{
-   User.findById
+   await User.findByIdAndUpdate(
+         req.user._id,
+         {
+            $set:{refreshToken:undefined}
+         },
+         {
+new:true
+         }
+
+    )
+    
+const options={
+    httpOnly:true,
+    secure:true
+}
+return res
+.status(200)
+.clearCookie("accessToken",options)
+.clearCookie("refreshToken",options)
+.json(new ApiResponse(200,{},"user logged out"))
+
 })
 
 export {registerUser,
