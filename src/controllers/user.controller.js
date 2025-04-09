d3c4d9e0fe2620ceddea7c63f69cc20fd4bede1c
Aman kumar <amankumar105541@gmail.com>
2025-04-09 22:45:39 +0530

    Debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -166,7 +166,7 @@ return res
 const refreshAccessToken= asynchandler(async(req,res)=>{
  const incomingRefreshToken=req.cookies.refreshToken ||req.body.refreshToken
 
- if(incomingRefreshToken){
+ if(!incomingRefreshToken){
     throw new ApiError(401,"unauthorized request")
  }
 
@@ -206,14 +206,15 @@ const refreshAccessToken= asynchandler(async(req,res)=>{
  } catch (error) {
     throw new ApiError(401,error?.message || "invalid refresh token")
  }
- 
+
 })
 
 
 
 export {registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
 
 
