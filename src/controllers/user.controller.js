d60f72d1b6d69b7aef07d2d01b6062481adf5aa7
Aman kumar <amankumar105541@gmail.com>
2025-04-10 10:18:38 +0530

    update text based controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -233,13 +233,39 @@ return res.status(200)
 .json(200,req.user,"current user fetched succesfully")
 })
 
+const updateAccountDetails= asynchandler(async(req,res)=>{
+const {fullName,email}=req.body
+
+if (!fullName || !email) {
+    throw new ApiError(400,"fiels are required")
+}
+
+const user= User.findByIdAndUpdate(
+    req.user?._id,
+    {
+        $set:{
+            fullName,
+            email
+        }
+    },
+    {new:true}
+).select("-password")
+
+return res
+.status(200)
+.json(new ApiResponse(200,user,"Account details updated sucesfully"))
+
+
+})
+
 export {registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    
+    updateAccountDetails,
+
 }
 
 
