5f5b3114966027d4ab267273150362bc25721db9
Aman kumar <amankumar105541@gmail.com>
2025-04-10 11:52:08 +0530

    Debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -230,7 +230,7 @@ return res
 
 const getCurrentUser= asynchandler(async(req,res)=>{
 return res.status(200)
-.json(200,req.user,"current user fetched succesfully")
+.json(new ApiResponse(200,req.user,"current user fetched succesfully"))
 })
 
 const updateAccountDetails= asynchandler(async(req,res)=>{
@@ -240,7 +240,7 @@ if (!fullName || !email) {
     throw new ApiError(400,"fiels are required")
 }
 
-const user= User.findByIdAndUpdate(
+const user= await User.findByIdAndUpdate(
     req.user?._id,
     {
         $set:{
